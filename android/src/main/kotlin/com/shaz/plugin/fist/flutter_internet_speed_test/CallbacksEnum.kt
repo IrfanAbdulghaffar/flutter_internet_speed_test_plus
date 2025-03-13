a7a9cad28fda030a061faf8e12a46b09c040e3f1
Irfan <irfan.abdulghaffar@schuja.com>
2025-03-13 12:14:19 +0500

    Initial commit

---
+++ android/src/main/kotlin/com/shaz/plugin/fist/flutter_internet_speed_test/CallbacksEnum.kt
@@ -0,0 +1,13 @@
+package com.shaz.plugin.fist.flutter_internet_speed_test_plus
+
+enum class CallbacksEnum {
+    START_DOWNLOAD_TESTING,
+    START_UPLOAD_TESTING
+}
+
+enum class ListenerEnum {
+    COMPLETE,
+    ERROR,
+    PROGRESS,
+    CANCEL
+}
